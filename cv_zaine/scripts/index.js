f308b66a856b12b8dbea572e686991a467da66f9
Zaine Queiros <zainequeiros@gmail.com>
2020-11-03 22:50:32 -0300

    Currículo feito em HTML, CSS e JS

---
+++ cv_zaine/scripts/index.js
@@ -0,0 +1,10 @@
+var button = document.getElementById('cv');
+var buttonSegundo = document.getElementById('email');
+
+button.onclick = () => {
+    window.open('https://drive.google.com/file/d/19L46ku6C1hW5PYZuNJoBOW8aYFUByWPf/view?usp=sharing');
+}
+
+buttonSegundo.onclick = () => {
+    window.open('mailto:zaqueiros@hotmail.com');
+}
